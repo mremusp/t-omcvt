296430a8a8a8a27fd67127c6ec9dbce1d364e455
Marius Pislaru <m.r.pislaru@gmail.com>
2022-07-25 08:10:51 +0300

    numberguess end

--- pr-1/script.js
+++ pr-1/script.js
@@ -1,42 +1,48 @@
 'use strict';
+// Modern JS course for everyone - Section 7: Project #1 - Guess my number
 
 let secretNumber = Math.trunc(Math.random() * 20 + 1);
 
 let score = 20;
+let highScore = 0;
+
+const displayMessage = function (message) {
+	document.querySelector('.message').textContent = message;
+};
 
 document.querySelector('.check').addEventListener('click', function () {
 	const guess = Number(document.querySelector('.guess').value);
 
 	if (!guess) {
-		document.querySelector('.message').textContent = '🚫 No number!';
+		displayMessage('🚫 No number!');
 	} else if (guess === secretNumber) {
-		document.querySelector('.message').textContent = '🎉 Correct Number!';
+		displayMessage('🎉 Correct Number!');
 		document.querySelector('.number').textContent = secretNumber;
 		document.querySelector('body').style.backgroundColor = '#60b347';
 		document.querySelector('.number').style.width = '30rem';
-	} else if (guess > secretNumber) {
-		if (score > 1) {
-			document.querySelector('.message').textContent = '👆 Too high!';
-			document.querySelector('.score').textContent = --score;
-		} else {
-			document.querySelector('.message').textContent = '💥 You lost the game!';
+
+		if (score > highScore) {
+			highScore = score;
+			document.querySelector('.highscore').textContent = highScore;
 		}
-	} else if (guess < secretNumber) {
+	} else if (guess !== secretNumber) {
 		if (score > 1) {
-			document.querySelector('.message').textContent = '👇 Too low!';
+			displayMessage(guess > secretNumber ? '👆 Too high!' : '👇 Too low!');
 			document.querySelector('.score').textContent = --score;
 		} else {
-			document.querySelector('.message').textContent = '💥 You lost the game!';
+			displayMessage('💥 You lost the game!');
 		}
 	}
 });
 
 document.querySelector('.again').addEventListener('click', function () {
 	secretNumber = Math.trunc(Math.random() * 20 + 1);
-	document.querySelector('.message').textContent = 'Start guessing...';
+	displayMessage('Start guessing...');
 	document.querySelector('.number').textContent = '?';
 	document.querySelector('body').style.backgroundColor = '#222';
 	document.querySelector('.number').style.width = '15rem';
-	document.querySelector('.score').textContent = '20';
+
+	score = 20;
+	document.querySelector('.score').textContent = score;
 	document.querySelector('.guess').value = '';
 });
