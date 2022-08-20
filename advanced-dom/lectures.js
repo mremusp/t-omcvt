e26b3a9b0a715ec7eb979611ad9f2ac2b8f8e33a
Marius Pislaru <m.r.pislaru@gmail.com>
2022-08-20 13:12:45 +0300

    adv DOM section part 2

--- advanced-dom/lectures.js
+++ advanced-dom/lectures.js
@@ -1,5 +1,5 @@
 /* 186, 187: select create delete sty;e attribute class */
-console.group('select create delete');
+console.groupCollapsed('select create delete');
 /* Selecting elements */
 console.log(document.documentElement, document.head, document.body); /* selects entire HTML element */
 
@@ -33,7 +33,7 @@ console.log(getComputedStyle(message).color); /* gets the style of an element, n
 
 message.style.height = Number.parseFloat(getComputedStyle(message).height) + 30 + 'px';
 
-document.documentElement.style.setProperty('--color-primary', 'orangered'); /* for custom properties, like css variables (or any css property) */
+// document.documentElement.style.setProperty('--color-primary', 'orangered'); /* for custom properties, like css variables (or any css property) */
 
 /* Attributes */
 const logo = document.querySelector('.nav__logo');
@@ -50,4 +50,77 @@ console.log(logo.dataset.versionNumber); /*dataset has attributes declared with 
 /* classes */
 // .add() .remove() .toggle() .contains()
 // logo.className = 'asdf'; /* overwrites all classes */
+console.groupEnd();
+
+/* 189 : Events and handlers */
+console.groupCollapsed('Events');
+const h1 = document.querySelector('h1');
+
+const alertH1 = function () {
+	console.log('addEventListener: reading heading');
+	h1.removeEventListener('mouseenter', alertH1); /* so it will only trigger once */
+};
+h1.addEventListener('mouseenter', alertH1);
+h1.onmouseenter = () => console.log('onMouseEnterL reading heading'); /* old way of addEventListener */
+h1.addEventListener('mouseenter', () =>
+	console.log('addEventListener second: reading heading')
+); /* addEventListener can bind multiple functions to same event */
+
+/* 191: event propagation */
+const randomInt = (min, max) => Math.floor(Math.random() * (max - min + 1) + min);
+
+const randomColor = () => `rgb(${randomInt(0, 255)},${randomInt(0, 255)},${randomInt(0, 255)})`;
+
+document.querySelector('.nav__link').addEventListener('click', function (e) {
+	// this.style.backgroundColor = randomColor();
+	// console.log('Link', e.target, e.currentTarget); /* .target is event target, .currentTarget is event which listens ("this") */
+	/* Stop propagation */
+	// e.stopPropagation();
+});
+document.querySelector('.nav__links').addEventListener('click', function (e) {
+	// this.style.backgroundColor = randomColor();
+	// console.log('Nav links', e.target, e.currentTarget);
+});
+document.querySelector('.nav').addEventListener(
+	'click',
+	function (e) {
+		// this.style.backgroundColor = randomColor();
+		// console.log('Header', e.target, e.currentTarget);
+	},
+	true /* calls function during capture, not bubbling */
+);
+console.groupEnd();
+
+/* 193: DOM Traversing */
+console.groupCollapsed('DOM Traversing');
+/* Going downwards */
+console.log(h1.querySelectorAll('.highlight'));
+console.log(h1.childNodes); /* returns NodeList with all elements (even plaintext and comments) */
+console.log(h1.children); /* returns HTML collection of actual HTML elements (only direct children) */
+console.log(h1.firstElementChild); /* first HTML element */
+console.log(h1.firstChild); /* first node (anything) */
+console.log(h1.lastElementChild);
+console.log(h1.lastChild);
+
+/* Going upwards */
+console.log(h1.parentNode); /* direct parent */
+console.log(h1.parentElement); /* direct parent which is HTML */
+// h1.closest('.header').style.background = 'var(--gradient-secondary)'; /* closest element, going up the DOM Tree */
+
+/* Going sideways (siblings) */
+console.log(h1.previousElementSibling); /* HTML elements */
+console.log(h1.nextElementSibling);
+console.log(h1.previousSibling); /* any elements */
+console.log(h1.nextSibling);
+
+console.log(h1.parentElement.children); /* all siblings */
+[...h1.parentElement.children].forEach((el) => {
+	// if (el !== h1) el.style.transform = 'scale(0.5)';
+});
+
+console.groupEnd();
+
+/*  */
+console.group('');
+
 console.groupEnd();
--- advanced-dom/script.js
+++ advanced-dom/script.js
@@ -7,6 +7,8 @@ const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
+const btnScrollTo = document.querySelector('.btn--scroll-to');
+const section1 = document.querySelector('#section--1');
 
 const openModal = function (e) {
 	e.preventDefault();
@@ -29,4 +31,61 @@ document.addEventListener('keydown', function (e) {
 	if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
 		closeModal();
 	}
+});
+
+btnScrollTo.addEventListener('click', (e) => {
+	const s1coords = section1.getBoundingClientRect(); /* gets coordinates and size of element relative to viewport*/
+	// console.log(s1coords);
+	// console.log('Current scroll (X/Y)', window.pageXOffset, window.pageYOffset);
+	// console.log('height/width viewport ', document.documentElement.clientHeight, document.documentElement.clientWidth);
+
+	/* Scrolling */
+	// window.scrollTo(s1coords.left + window.pageXOffset, s1coords.top + window.pageYOffset);
+	// window.scrollTo({
+	// 	left: s1coords.left + window.pageXOffset,
+	// 	top: s1coords.top + window.pageYOffset,
+	// 	behavior: 'smooth',
+	// });
+
+	/* simpler way (modern browsers) */
+	section1.scrollIntoView({ behavior: 'smooth' });
+});
+
+// document.querySelectorAll('.nav__link').forEach((el) =>
+// 	el.addEventListener('click', function (e) {
+// 		e.preventDefault();
+// 		const id = this.getAttribute('href'); /* this.href would get absolute URL */
+// 		console.log(id);
+// 		document.querySelector(id).scrollIntoView({ behavior: 'smooth' });
+// 	})
+// );
+
+document.querySelector('.nav__links').addEventListener('click', function (e) {
+	/* above is not efficient, put event listener on parent element, get current clicked item with e.target */
+	e.preventDefault();
+	if (e.target.classList.contains('nav__link')) {
+		const id = e.target.getAttribute('href');
+		document.querySelector(id).scrollIntoView({ behavior: 'smooth' });
+	}
+});
+
+const tabs = document.querySelectorAll('.operations__tab');
+const tabsContainer = document.querySelector('.operations__tab-container');
+const tabsContent = document.querySelectorAll('.operations__content');
+
+tabsContainer.addEventListener('click', function (e) {
+	e.preventDefault();
+	const clicked = e.target.closest('.operations__tab');
+	if (!clicked) return; /* exits function if not clicked on operations__tab */
+
+	if ([...tabs].some((tab) => tab === clicked)) {
+		console.log('oui');
+		tabs.forEach((tab) => tab.classList.remove('operations__tab--active'));
+		clicked.classList.add('operations__tab--active');
+
+		tabsContent.forEach((tab) => tab.classList.remove('operations__content--active'));
+		document.querySelector(`.operations__content--${clicked.dataset.tab}`).classList.add('operations__content--active');
+	}
+
+	// document.querySelector(`.operations__content--${clicked.getAttribute(tab)}`);
 });
