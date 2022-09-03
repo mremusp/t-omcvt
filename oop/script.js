1b9b1e619db4f8416090f78c9bc4b1d41215345f
Marius Pislaru <m.r.pislaru@gmail.com>
2022-09-03 14:26:30 +0300

    oop p2

--- oop/script.js
+++ oop/script.js
@@ -5,12 +5,12 @@ console.group('constructor functions');
 const Person = function (firstName, birthYear) {
 	/* convention: start constructor with uppercase letter */
 	this.firstName = firstName;
-	this.brithYear = birthYear;
+	this.birthYear = birthYear;
 
 	/* do not do this: */
-	this.calcAge = function () {
-		console.log(2037 - birthYear);
-	};
+	// this.calcAge = function () {
+	// 	console.log(2037 - birthYear);
+	// };
 }; /* arrow functions don't work as constructors (need this keyword) */
 
 const marius = new Person('Marius', 1993);
@@ -25,4 +25,21 @@ console.log(marius instanceof Person); /* checks if object is built with constru
 
 const matilde = new Person('Matilde', 2001);
 const jack = new Person('Jack', 1992);
+console.groupEnd();
+
+/* 209: prototypes */
+console.group('Prototypes');
+Person.prototype.calcAge = function () {
+	console.log(2038 - this.birthYear);
+};
+
+marius.calcAge();
+console.log(Person.prototype); /* prototype of linked objects */
+console.log(marius.__proto__); /* prototype of object instance */
+
+Person.prototype.species = 'Homo Sapiens';
+console.log(marius.species, jack.species);
+console.log(marius.hasOwnProperty('firstName')); /* property of object */
+console.log(marius.hasOwnProperty('species')); /* property of prototype */
+
 console.groupEnd();
